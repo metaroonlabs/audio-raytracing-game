91631b104f1e2befe144385953071d753b081dd8
Antimundo <antimundo21@gmail.com>
2025-08-30 15:18:50 +0200

    Update health sliders to show health number

--- scenes/target/health_slider/health_slider.gd
+++ scenes/target/health_slider/health_slider.gd
@@ -1,17 +1,12 @@
 class_name HealthSlider
-extends MeshInstance3D
+extends Sprite3D
 ## Slider that shows the health of the enemy targets
-
-func _ready() -> void:
-	var new_mesh: Mesh = get_mesh().duplicate()
-	set_mesh(new_mesh)
-	var new_material: Material = get_mesh().get_material().duplicate()
-	get_mesh().set_material(new_material)
 
 func _process(_delta: float) -> void:
 	var health = $"..".health
 	var max_health = $"..".max_health
-	scale.x = lerp(0, 10, health / max_health)
+	$SubViewport/ProgressBar.value = lerp(0, 100, health / max_health)
+	$SubViewport/ProgressBar/Label.text = str(health)
 
 func _on_target_hitted() -> void:
 	$AudioStreamPlayer.play()

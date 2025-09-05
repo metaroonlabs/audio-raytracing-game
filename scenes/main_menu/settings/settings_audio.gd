6fa68edd47ee572491f5218e401ede66350eb974
Antimundo <antimundo21@gmail.com>
2025-09-05 16:07:16 +0200

    Fix audio slider not working

--- scenes/main_menu/settings/settings_audio.gd
+++ scenes/main_menu/settings/settings_audio.gd
@@ -17,14 +17,8 @@ func _add_destroy_sounds(options_button: OptionButton):
 		i += 1
 
 func _on_volume_slider_value_changed(value) -> void:
-	SaveManager.settings.set_data("user", "volume", value)
+	SaveManager.settings.set_data("audio", "volume", value)
 	_set_volume_label()
-
-func _set_volume_label() -> void:
-	var volume_label := $VolumeLabel
-	volume_label.text = "Volume " + str(SaveManager.settings.get_data("audio", "volume"))
-	var volume_slider := $VolumeSlider
-	volume_slider.value = SaveManager.settings.get_data("audio", "volume")
 
 func _on_option_button_item_selected(index: int) -> void:
 	SaveManager.settings.set_data("audio", "hit_sound", option_button.get_item_metadata(index))
@@ -32,6 +26,12 @@ func _on_option_button_item_selected(index: int) -> void:
 
 func _on_preview_pressed() -> void:
 	$Preview/AudioStreamPlayer.play()
+
+func _set_volume_label() -> void:
+	var volume_label := $VolumeLabel
+	volume_label.text = "Volume " + str(SaveManager.settings.get_data("audio", "volume"))
+	var volume_slider := $VolumeSlider
+	volume_slider.value = SaveManager.settings.get_data("audio", "volume")
 
 func _set_selected_sound_index() -> void:
 	var selected = SaveManager.settings.get_data("audio", "hit_sound")

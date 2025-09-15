68216095592de94eed9433a34b994456f65a68b9
shrampe <shrampe@zaclys.net>
2025-09-15 23:01:37 +0200

    Pressing esc now exits the pause menu

--- scenes/game_world/pause_ui/pause.gd
+++ scenes/game_world/pause_ui/pause.gd
@@ -1,7 +1,9 @@
 extends CanvasLayer
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_cancel") && get_tree().paused:
+		trigger_pause(false)
+	elif event.is_action_pressed("ui_cancel") && !get_tree().paused:
 		trigger_pause(true)
 
 func _notification(what: int) -> void:

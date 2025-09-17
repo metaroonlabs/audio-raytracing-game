68fd5153c338be3111a0b9f334aee52302bc1ac0
Antimundo <antimundo21@gmail.com>
2025-09-17 19:27:46 +0200

    Remove decimals from FPS counter

--- scenes/autoload/overlay_ui_label.gd
+++ scenes/autoload/overlay_ui_label.gd
@@ -3,6 +3,6 @@ extends Label
 
 func _process(_delta: float) -> void:
 	if SaveManager.settings.get_data("user", "fps_overlay"):
-		set_text("FPS " + str(Engine.get_frames_per_second()))
+		set_text("FPS: %.0f" % Engine.get_frames_per_second())
 	else:
 		set_text("")

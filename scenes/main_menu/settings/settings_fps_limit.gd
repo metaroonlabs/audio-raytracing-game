911a6786504cc1b251b9ee56f933fae590c03668
Antimundo <antimundo21@gmail.com>
2025-09-17 23:49:00 +0200

    Add option to change the skybox color

--- scenes/main_menu/settings/settings_fps_limit.gd
+++ scenes/main_menu/settings/settings_fps_limit.gd
@@ -1,18 +1,17 @@
-extends VBoxContainer
+extends Control
 ## FPS limit settings
 
-@onready var fps_limit_slider = $FPSLimitSlider
-@onready var fps_limit_label = $FPSLimitLabel
+@onready var _fps_limit_slider: HSlider = $FPSLimitSlider
+@onready var _fps_limit_label: Label = $FPSLimitLabel
+
+func _ready() -> void:
+	_fps_limit_slider.value = SaveManager.settings.get_data("video", "fps_limit")
+	update_label()
 
 func _on_fps_limit_slider_value_changed(value) -> void:
 	SaveManager.settings.set_data("video", "fps_limit", value)
 	DisplayManager.set_max_fps(value)
 	update_label()
 
-func _ready() -> void:
-	fps_limit_slider.value = SaveManager.settings.get_data("video", "fps_limit")
-	
-	update_label()
-
 func update_label() -> void:
-	fps_limit_label.text = "FPS Limit: %d fps" % fps_limit_slider.value 
+	_fps_limit_label.text = "FPS Limit: %d fps" % _fps_limit_slider.value 
--- scenes/main_menu/settings/settings_world_appareance.gd
+++ scenes/main_menu/settings/settings_world_appareance.gd
@@ -1,43 +1,49 @@
-extends VBoxContainer
+extends Node
 ## World appareance settings
 
-@onready var world_color = $WorldColor/Color
-@onready var world_texture = $WorldTexture
-@onready var target_color = $TargetColor/TargetColor
-@onready var preview = $Preview
-@onready var target_preview = $Preview/TargetPreview
-
-func _on_color_color_changed(color: Color) -> void:
-	SaveManager.settings.set_data("world", "world_color", color)
-	update_preview()
-
-func _on_target_color_color_changed(color: Color) -> void:
-	SaveManager.settings.set_data("world", "target_color", color)
-	update_preview()
-
-func _on_world_texture_item_selected(index) -> void:
-	SaveManager.settings.set_data("world", "world_texture", world_texture.get_item_metadata(index))
-	update_preview()
+@onready var _world_color: ColorPickerButton = $WorldColor/Color
+@onready var _world_texture: OptionButton = $WorldTexture
+@onready var _target_color: ColorPickerButton = $TargetColor/TargetColor
+@onready var _preview: TextureRect = $Preview
+@onready var _target_preview = $Preview/TargetPreview
+@onready var _ambient_color: ColorPickerButton = $AmbientColor/Color
 
 func _ready() -> void:
 	var i = 0
 	for path: String in Global.get_world_textures():
-		world_texture.add_icon_item(CustomResourceManager.get_image(path), path.get_file(), i)
-		world_texture.set_item_metadata(i, path)
+		_world_texture.add_icon_item(CustomResourceManager.get_image(path), path.get_file(), i)
+		_world_texture.set_item_metadata(i, path)
 		i+=1
-	world_color.color = SaveManager.settings.get_data("world", "world_color")
-	target_color.color = SaveManager.settings.get_data("world", "target_color")
-	get_selected_texture_index()
-	update_preview()
+	_world_color.color = SaveManager.settings.get_data("world", "world_color")
+	_target_color.color = SaveManager.settings.get_data("world", "target_color")
+	_ambient_color.color = SaveManager.settings.get_data("world", "ambient_color")
+	_get_selected_texture_index()
+	_update_preview()
 
-func get_selected_texture_index() -> void:
+func _on_color_color_changed(color: Color) -> void:
+	SaveManager.settings.set_data("world", "world_color", color)
+	_update_preview()
+
+func _on_target_color_color_changed(color: Color) -> void:
+	SaveManager.settings.set_data("world", "target_color", color)
+	_update_preview()
+
+func _on_world_texture_item_selected(index) -> void:
+	SaveManager.settings.set_data("world", "world_texture", _world_texture.get_item_metadata(index))
+	_update_preview()
+
+func _on_amient_color_changed(color: Color) -> void:
+	SaveManager.settings.set_data("world", "ambient_color", color)
+	_update_preview()
+
+func _get_selected_texture_index() -> void:
 	var selected = SaveManager.settings.get_data("world", "world_texture")
-	for i in range(world_texture.item_count):
-		if selected == world_texture.get_item_metadata(i):
-			world_texture.select(i)
-	update_preview()
+	for i in range(_world_texture.item_count):
+		if selected == _world_texture.get_item_metadata(i):
+			_world_texture.select(i)
+	_update_preview()
 
-func update_preview() -> void:
-	preview.texture = Global.get_current_world_texture()
-	preview.self_modulate = SaveManager.settings.get_data("world", "world_color")
-	target_preview.color = SaveManager.settings.get_data("world", "target_color")
+func _update_preview() -> void:
+	_preview.texture = Global.get_current_world_texture()
+	_preview.self_modulate = SaveManager.settings.get_data("world", "world_color")
+	_target_preview.color = SaveManager.settings.get_data("world", "target_color")
